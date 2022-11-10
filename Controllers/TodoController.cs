0f1576ad1534221883e4ba34a66554441ee5bd8d
EdsonSantosCode <dev.escode@hotmail.com>
2022-11-10 06:45:57 -0300

    Criado put e delete

--- Controllers/TodoController.cs
+++ Controllers/TodoController.cs
@@ -14,7 +14,19 @@ public class TodoController : ControllerBase
         var ret = context.Todos.ToList();
 
         if (ret.Count <= 0)
-            return NoContent();
+            return NotFound();
+
+        return Ok(ret);
+    }
+
+    [HttpGet]
+    [Route("/{id:int}")]
+    public IActionResult Get(int id, [FromServices] Context context)
+    {
+        var ret = context.Todos.FirstOrDefault(x => x.Id == id);
+
+        if (ret is null)
+            return NotFound();
 
         return Ok(ret);
     }
@@ -30,5 +42,44 @@ public class TodoController : ControllerBase
             return BadRequest();
 
         return Ok(todo);
+    }
+
+    [HttpPut]
+    [Route("/{id:int}")]
+    public IActionResult Put(int id, [FromBody] TodoModel todo, [FromServices] Context context)
+    {
+        var model = context.Todos.FirstOrDefault(x => x.Id == id);
+
+        if (model is null)
+            return NotFound();
+
+        model.Title = todo.Title;
+        model.Done = todo.Done;
+
+        context.Todos.Update(model);
+        var persist = context.SaveChanges();
+
+        if (persist == 0)
+            return BadRequest();
+
+        return Ok(model);
+    }
+
+    [HttpDelete]
+    [Route("/{id:int}")]
+    public IActionResult Delete(int id, [FromServices] Context context)
+    {
+        var model = context.Todos.FirstOrDefault(x => x.Id == id);
+
+        if (model is null)
+            return NotFound();
+
+        context.Todos.Remove(model);
+        var persist = context.SaveChanges();
+
+        if (persist == 0)
+            return BadRequest();
+
+        return Ok();
     }
 }
